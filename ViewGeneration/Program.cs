e030e94b67d4d1329a4418b102ff592319625c74
aleksey ignatyev <a.ignatyev@abbyy-ls.com>
2017-03-16 07:57:40 +0300

    Revert "del"
    This reverts commit 700d59b794a9f6755c1cade4f080cd8ab60dfffc.

---
+++ ViewGeneration/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Extreme.Net;
+using OpenQA.Selenium.Internal;
+using ViewBot;
+
+namespace ViewGeneration
+{
+	class Program
+	{
+		private static string _selfIp;
+		private static readonly Regex _reges = new Regex("\\d+.\\d+.\\d+.\\d+");
+
+		static void Main(string[] args)
+		{
+			_selfIp = getIp(null).Result;
+
+			closeAllChromeInstances();
+
+			var proxyList = Proxies.GetGoodProxies();
+
+			Console.WriteLine("Начинаю работу");
+
+			var engine = new Engine(new List<Searcher>
+				{
+					new Searcher
+					{
+						YandexUrl = "https://yandex.ru/search/?text=%D0%BF%D0%B8%D0%BB%D0%BE%D1%82%D0%BD%D0%B0%D1%8F%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C%20%D0%B2%20%D0%B1%D0%BB%D0%BE%D0%B3%D0%B5&lr=213&p=4",
+						ElementToClick = "//a[text()=' | BrutalEveryDay']"
+					},
+
+					new Searcher()
+					{
+						YandexUrl = "https://yandex.ru/search/?msid=1489506224.20821.20941.27535&text=%D0%BC%D1%83%D0%B7%D0%B5%D0%B9%20%D0%B2%D0%BE%D1%81%D1%81%D1%82%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%BC%D0%B0%D1%88%D0%B8%D0%BD&lr=213&p=20",
+						ElementToClick = "(//b[text()= 'Восстание'])[1]"
+					},
+
+					//new Searcher
+					//{
+					//	YandexUrl = "https://yandex.ru/search/?text=work%20and%20travel%20%D1%81%D1%88%D0%B0&lr=10738&p=5",
+					//	ElementToClick = "//a[text()=': Что взять с собой в ']"
+					//},
+
+					//new Searcher()
+					//{
+					//	YandexUrl = "https://yandex.ru/search/?text=work%20and%20travel%20%D0%B2%D0%B8%D0%B7%D0%B0&lr=213&p=3",
+					//	ElementToClick = "//a[text()='Writetravel | Блог о ']"
+					//},
+
+					//new Searcher()
+					//{
+					//	YandexUrl = "https://yandex.ru/search/?text=working%20travel&lr=213&p=6",
+					//	ElementToClick = "//a[text()='Writetravel | Блог о ']"
+					//},
+
+				new Searcher()
+					{
+						YandexUrl = "https://vk.com/away.php?to=http%3A%2F%2Fbrutaleveryday.ru%2Fkak-poxudet-pravilnoe-pitanie%2F&post=401123499_8&el=snippet",
+						ElementToClick = "//a[@href='http://brutaleveryday.ru/kak-poxudet/']"
+					},
+
+				new Searcher()
+				{
+					YandexUrl = "https://vk.com/away.php?to=http%3A%2F%2Fbrutaleveryday.ru%2Fkak-poxudet%2F&post=401123499_10&el=snippet",
+					ElementToClick = "//a[@href='http://brutaleveryday.ru/kak-poxudet-pravilnoe-pitanie/']"
+				}
+				});
+
+
+			var sw = new Stopwatch();
+			sw.Start();
+
+			ParallelOptions opt = new ParallelOptions();
+			ThreadPool.SetMaxThreads(25, 25);
+
+			bool isRunning = false;
+
+			while (true)
+			{
+				if (!isRunning)
+				{
+					isRunning = true;
+					Parallel.ForEach(proxyList, (prox, state) =>
+					{
+						string proxyIp = string.Empty;
+
+						try
+						{
+							proxyIp = getIp(prox).Result;
+						}
+						catch (Exception ex)
+						{
+							return;
+						}
+
+						if (sw.Elapsed.Minutes > 30)
+						{
+							isRunning = false;
+							closeAllChromeInstances();
+							sw.Restart();
+							state.Break();
+						}
+
+						if (proxyIp != string.Empty && proxyIp != _selfIp)
+							ThreadPool.QueueUserWorkItem(engine.DoWork, prox);
+					});
+				}
+			}
+		}
+
+		private static void closeAllChromeInstances()
+		{
+			var drivers = Process.GetProcessesByName("chromedriver");
+
+			foreach (var driver in drivers)
+			{
+				try
+				{
+					driver.Kill();
+				}
+				catch (Exception)
+				{ 
+				}
+			}
+
+			var chromes = Process.GetProcessesByName("chrome");
+
+			foreach (var chrome in chromes)
+			{
+				try
+				{
+					chrome.Kill();
+				}
+				catch (Exception)
+				{
+
+				}
+				
+			}
+		}
+
+		private static async Task<string> getIp(ProxyClient proxy)
+		{
+			ProxyHandler handler = new ProxyHandler(proxy);
+
+			using (HttpClient client = new HttpClient(handler))
+			{
+				var direction = client.GetStringAsync("https://www.check-my-ip.net/").Result;
+
+				var ss = direction.Split(new[] { "<h1>", " - " }, StringSplitOptions.RemoveEmptyEntries);
+
+				foreach (var line in ss)
+				{
+					if (_reges.IsMatch(line))
+						return line;
+				}
+
+				return _selfIp;
+			}
+		}
+
+		private static bool checkProxy(ProxyClient proxy)
+		{
+			var proxyIp = getIp(proxy).Result;
+
+			if (proxyIp == _selfIp)
+				return false;
+			
+			return true;
+		}
+	}
+}
---
+++ ViewGeneration/Proxies.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Extreme.Net;
+
+namespace ViewBot
+{
+	internal static class Proxies
+	{
+		public static List<ProxyClient> ProxyList = new List<ProxyClient>();
+		public static List<string> GoodProxyList = new List<string>();
+
+		private static List<ProxyClient> getProxiesFromFile(ProxyType type, string fileName)
+		{
+			List<ProxyClient> proxiesList = new List<ProxyClient>();
+
+			foreach (var line in File.ReadAllLines(fileName))
+			{
+				ProxyClient client;
+
+				try
+				{
+					client = ProxyClient.Parse(type, line.Trim());
+				}
+
+				catch (Exception)
+				{
+					continue;
+				}
+
+				if (!proxiesList.Contains(client))
+					proxiesList.Add(client);
+
+			}
+
+			return proxiesList;
+		}
+
+		private static void WriteInFileGoodProxy()
+		{
+			GoodProxyList.Add(DateTime.Now.ToLongTimeString());
+
+			File.WriteAllLines("GoodProxySocks5.txt", GoodProxyList.ToArray());
+		}
+
+		public static List<ProxyClient> GetGoodProxies()
+		{
+			return getProxiesFromFile(ProxyType.Http, @"C:\viewGeneraion2\Proxies\fileForTopProxy.txt");
+		}
+
+		public static void GetProxies()
+		{
+			//ProxyList.AddRange(getProxiesFromFile(ProxyType.Http, "GoodProxySocks4.txt"));
+			//ProxyList.AddRange(getProxiesFromFile(ProxyType.Socks4, "Socks4.txt"));
+			ProxyList.AddRange(getProxiesFromFile(ProxyType.Http, @"fileForTopProxy.txt"));
+		}
+	}
+}
+
+	
+
